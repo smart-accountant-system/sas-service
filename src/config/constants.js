e3d04c3641355aef76584da7ab5753371191a299
Quang Truong <nhatquangsin@gmail.com>
2019-05-18 18:50:26 +0700

    deploy update

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,7 +5,7 @@ const devConfig = {
 
 const prodConfig = {
   // MONGO_URL: 'mongodb://thienhaco:22196c97c6110f2c04525395ca8394a7@localhost:27017/SAS-service',
-  MONGO_URL: 'mongodb://nhatquangsin:Admin!123@ds159036.mlab.com:59036/sasdb',
+  MONGO_URL: 'mongodb://nhatquangsin:nhatquang98@ds159036.mlab.com:59036/sasdb',
   JWT_SECRET: 'sai-co-pat',
 };
 
