a844e6cc07a97dbf5213fba565e2c9c9eb855d51
Đức Thọ Trần <ducthotran2010@gmail.com>
2019-05-20 19:58:36 +0700

    Update routes

--- src/module/index.js
+++ src/module/index.js
@@ -1,5 +1,7 @@
 import userRoutes from './user/user.route';
+import companyRoutes from './company/company.route';
 
 export default app => {
   app.use('/user', userRoutes);
+  app.use('/company', companyRoutes);
 };
