3046729e67945ad13254703f95e600d79878900e
Đức Thọ Trần <ducthotran2010@gmail.com>
2019-05-29 12:53:24 +0700

    Rename role Employee to Staff

--- src/module/customer/customer.route.js
+++ src/module/customer/customer.route.js
@@ -3,14 +3,14 @@ import validate from 'express-validation';
 import * as customerController from './customer.controller';
 import customerValidation from './customer.validation';
 import { authJwt } from '../../service/passport';
-import { roleNOTA, roleEmployee } from '../../service/role';
+import { roleNOTA, roleStaff } from '../../service/role';
 
 const routes = new Router();
 
 routes.get('/', authJwt, roleNOTA, customerController.getCustomerList);
 routes.get('/:id', authJwt, roleNOTA, customerController.getDetailCustomer);
-routes.post('/', authJwt, roleEmployee, validate(customerValidation.createCustomer), customerController.createCustomer);
-routes.patch('/:id', authJwt, roleEmployee, validate(customerValidation.editCustomer), customerController.updateCustomer);
-routes.delete('/:id', authJwt, roleEmployee, customerController.deleteCustomer);
+routes.post('/', authJwt, roleStaff, validate(customerValidation.createCustomer), customerController.createCustomer);
+routes.patch('/:id', authJwt, roleStaff, validate(customerValidation.editCustomer), customerController.updateCustomer);
+routes.delete('/:id', authJwt, roleStaff, customerController.deleteCustomer);
 
 export default routes;
--- src/module/invoice/invoice.route.js
+++ src/module/invoice/invoice.route.js
@@ -3,14 +3,14 @@ import validate from 'express-validation';
 import * as invoiceController from './invoice.controller';
 import invoiceValidation from './invoice.validation';
 import { authJwt } from '../../service/passport';
-import { roleNOTA, roleEmployee } from '../../service/role';
+import { roleNOTA, roleStaff } from '../../service/role';
 
 const routes = new Router();
 
 routes.get('/', authJwt, roleNOTA, invoiceController.getInvoiceList);
 routes.get('/:id', authJwt, roleNOTA, invoiceController.getDetailInvoice);
-routes.post('/', authJwt, roleEmployee, validate(invoiceValidation.createInvoice), invoiceController.createInvoice);
-routes.patch('/:id', authJwt, roleEmployee, validate(invoiceValidation.editInvoice), invoiceController.updateInvoice);
-routes.delete('/:id', authJwt, roleEmployee, invoiceController.deleteInvoice);
+routes.post('/', authJwt, roleStaff, validate(invoiceValidation.createInvoice), invoiceController.createInvoice);
+routes.patch('/:id', authJwt, roleStaff, validate(invoiceValidation.editInvoice), invoiceController.updateInvoice);
+routes.delete('/:id', authJwt, roleStaff, invoiceController.deleteInvoice);
 
 export default routes;
