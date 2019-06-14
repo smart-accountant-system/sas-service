a9910f6cfbed60f7d9fe45921893bee4f9d120d8
Bảo Hồ <ducthotran2010@gmail.com>
2019-06-15 00:02:11 +0700

    Fix dashboard from always return 0

--- src/module/dashboard/dashboard.controller.js
+++ src/module/dashboard/dashboard.controller.js
@@ -5,9 +5,9 @@ import Customer from '../customer/customer.model';
 
 export async function getDashboard(req, res) {
   try {
-    const totalCategory = await Category.count({ isRemove: false, company: req.user.company });
-    const totalEmployee = await Employee.count({ isRemove: false, company: req.user.company });
-    const totalCustomer = await Customer.count({ isRemove: false, company: req.user.company });
+    const totalCategory = await Category.count({ isRemoved: false, company: req.user.company });
+    const totalEmployee = await Employee.count({ isRemoved: false, company: req.user.company });
+    const totalCustomer = await Customer.count({ isRemoved: false, company: req.user.company });
     return res.status(HTTPStatus.OK).json({ totalCategory, totalEmployee, totalCustomer });
   } catch (e) {
     return res.status(HTTPStatus.BAD_REQUEST).json(e.message);
