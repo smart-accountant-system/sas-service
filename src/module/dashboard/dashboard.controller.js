db8065364f244c225c08b9f34d5b5784ffad39eb
Bảo Hồ <ducthotran2010@gmail.com>
2019-06-14 23:24:39 +0700

    Add dashboard information

---
+++ src/module/dashboard/dashboard.controller.js
@@ -0,0 +1,15 @@
+import HTTPStatus from 'http-status';
+import Category from '../payment-category/category.model';
+import Employee from '../employee/employee.model';
+import Customer from '../customer/customer.model';
+
+export async function getDashboard(req, res) {
+  try {
+    const totalCategory = await Category.count({ isRemove: false, company: req.user.company });
+    const totalEmployee = await Employee.count({ isRemove: false, company: req.user.company });
+    const totalCustomer = await Customer.count({ isRemove: false, company: req.user.company });
+    return res.status(HTTPStatus.OK).json({ totalCategory, totalEmployee, totalCustomer });
+  } catch (e) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(e.message);
+  }
+}
