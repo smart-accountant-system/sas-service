fee5949e1becf9f18ec5abc6c0eb0599cda9000a
Bảo Hồ <hqb@Chu-Bao.net.fpt>
2019-06-14 23:24:39 +0700

    Add dashboard information

---
+++ src/module/dashboard/dashboard.route.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import validate from 'express-validation';
+import * as dashboardController from './dashboard.controller';
+import { authJwt } from '../../service/passport';
+import { roleNOTA, roleNOTS } from '../../service/role';
+
+const routes = new Router();
+
+routes.get('/', authJwt, roleNOTA, roleNOTS, dashboardController.getDashboard);
+
+export default routes;
