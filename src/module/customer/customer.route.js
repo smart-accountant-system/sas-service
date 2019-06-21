ac587b62fb0a6567c5161af2b17a0550fa1a7df4
Bảo Hồ <ducthotran2010@gmail.com>
2019-06-21 11:56:59 +0700

    Allow manager to manage customer

--- src/module/customer/customer.route.js
+++ src/module/customer/customer.route.js
@@ -3,14 +3,14 @@ import validate from 'express-validation';
 import * as customerController from './customer.controller';
 import customerValidation from './customer.validation';
 import { authJwt } from '../../service/passport';
-import { roleNOTA, roleStaff } from '../../service/role';
+import { roleNOTA, roleNOTA2 } from '../../service/role';
 
 const routes = new Router();
 
 routes.get('/', authJwt, roleNOTA, customerController.getCustomerList);
 routes.get('/:id', authJwt, roleNOTA, customerController.getDetailCustomer);
-routes.post('/', authJwt, roleStaff, validate(customerValidation.createCustomer), customerController.createCustomer);
-routes.patch('/:id', authJwt, roleStaff, validate(customerValidation.editCustomer), customerController.updateCustomer);
-routes.delete('/:id', authJwt, roleStaff, customerController.deleteCustomer);
+routes.post('/', authJwt, roleNOTA2, validate(customerValidation.createCustomer), customerController.createCustomer);
+routes.patch('/:id', authJwt, roleNOTA2, validate(customerValidation.editCustomer), customerController.updateCustomer);
+routes.delete('/:id', authJwt, roleNOTA2, customerController.deleteCustomer);
 
 export default routes;
