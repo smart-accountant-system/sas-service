2e11a94e525aa184d52fbb2de786bf86bbad9657
Bảo Hồ <ducthotran2010@gmail.com>
2019-06-25 10:13:02 +0700

    Update routee payment

--- src/module/payment/payment.route.js
+++ src/module/payment/payment.route.js
@@ -7,7 +7,7 @@ import { roleNOTA, roleStaff } from '../../service/role';
 
 const routes = new Router();
 
-routes.get('/:invoice', authJwt, roleNOTA, paymentController.getPaymentList);
+routes.get('/invoices/:invoice', authJwt, roleNOTA, paymentController.getPaymentList);
 routes.get('/:id', authJwt, roleNOTA, paymentController.getDetailPayment);
 routes.post('/', authJwt, roleStaff, validate(paymentValidation.createPayment), paymentController.createPayment);
 routes.delete('/:id', authJwt, roleStaff, paymentController.deletePayment);
