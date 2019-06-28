9b094f8eeebe999318702784ca506819f0ff5885
Bảo Hồ <ducthotran2010@gmail.com>
2019-06-29 00:55:17 +0700

    Add role contraint for dashboard

--- src/module/dashboard/dashboard.controller.js
+++ src/module/dashboard/dashboard.controller.js
@@ -2,11 +2,13 @@ import HTTPStatus from 'http-status';
 import Category from '../payment-category/category.model';
 import Employee from '../employee/employee.model';
 import Customer from '../customer/customer.model';
+import contants from '../../config/constants';
 
 export async function getDashboard(req, res) {
   try {
     const totalCategory = await Category.count({ isRemoved: false, company: req.user.company });
-    const totalEmployee = await Employee.count({ isRemoved: false, company: req.user.company });
+    const totalEmployee = (req.user.role !== contants.ROLE.ACCOUNTANT) ? 
+      await Employee.count({ isRemoved: false, company: req.user.company }) : undefined;
     const totalCustomer = await Customer.count({ isRemoved: false, company: req.user.company });
     return res.status(HTTPStatus.OK).json({ totalCategory, totalEmployee, totalCustomer });
   } catch (e) {
