ffdcf7480591c77e966e61e879c439c7ce7e57c0
Quang Truong <quangtruong@Quangs-MacBook-Pro.local>
2019-07-03 16:36:12 +0700

    hack.........

--- src/service/file.js
+++ src/service/file.js
@@ -4,9 +4,11 @@ import path from 'path';
 export default multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
+      console.log('des= ', file);
       done(null, path.join('upload_storage'));
     },
     filename(req, file, done) {
+      console.log('filename ', file);
       done(null, 
         `SAS-${
           file.originalname.includes('SAS') ? 
