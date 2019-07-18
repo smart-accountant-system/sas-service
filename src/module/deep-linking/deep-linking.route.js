31d061426920286dcbd7ef60df6e23c1b146e7e7
Đức Thọ Trần <ducthotran2010@gmail.com>
2019-07-19 00:50:23 +0700

    Happy life v2

--- src/module/deep-linking/deep-linking.route.js
+++ src/module/deep-linking/deep-linking.route.js
@@ -3,9 +3,19 @@ import constants from '../../config/constants';
 
 const routes = new Router();
 
-routes.get('/:token', (req, res) => {
+routes.get('/expo-publish/:token', (req, res) => {
   const token = req.params.token;
   return res.redirect(constants.PUBLISH_EXPO_LINK + token);
+});
+
+routes.get('/expo-debug/:token', (req, res) => {
+  const token = req.params.token;
+  return res.redirect(constants.DEBUG_EXPO_LINK + token);
+});
+
+routes.get('/release/:token', (req, res) => {
+  const token = req.params.token;
+  return res.redirect(constants.RELEASE_EXPO_LINK + token);
 });
 
 export default routes;
